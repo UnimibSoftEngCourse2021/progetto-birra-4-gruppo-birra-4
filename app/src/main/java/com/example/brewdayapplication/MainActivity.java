e85a2680e93607f9dc2d0543bf031a5ddda4c7ad
Alessandro Masciocchi <a.masciocchi@campus.unimib.it>
2021-01-04 17:15:38 +0100

    commento Ale

--- app/src/main/java/com/example/brewdayapplication/MainActivity.java
+++ app/src/main/java/com/example/brewdayapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*commento prova Jury*/
         /*commento prova lara*/
+        /* commento Ale*/
     }
 }
