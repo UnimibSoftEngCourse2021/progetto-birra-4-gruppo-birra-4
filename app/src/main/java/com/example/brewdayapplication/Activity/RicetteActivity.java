dc9e4c3173efc36190bca29f95c2f1c89e8e05f6
LaraCugnoParrotta <l.cugnoparrotta@campus.unimib.it>
2021-01-11 13:17:09 +0100

    creata dialog per creazione ricetta con relativo layout

--- app/src/main/java/com/example/brewdayapplication/Activity/RicetteActivity.java
+++ app/src/main/java/com/example/brewdayapplication/Activity/RicetteActivity.java
@@ -1,7 +1,12 @@
 package com.example.brewdayapplication.Activity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +20,7 @@ public class RicetteActivity extends AppCompatActivity {
     DatabaseManager databaseManager;
     ListView listViewRicette;
     FloatingActionButton aggiungiRicetta;
+    Dialog dialogRicetta;
 
 
     @Override
@@ -26,8 +32,16 @@ public class RicetteActivity extends AppCompatActivity {
         listViewRicette = findViewById(R.id.layoutListaRicette);
         aggiungiRicetta = findViewById(R.id.addRicetta);
 
-    }
+        dialogRicetta = new Dialog(this);
+        aggiungiRicetta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogRicetta.setContentView(R.layout.activity_dialog_new_ricetta);
+                dialogRicetta.show();
+            }
+        });
 
+    }
 
 
 }
